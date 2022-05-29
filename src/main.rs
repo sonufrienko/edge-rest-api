dfada1867733c37b887678faa8638d12a154ad77
Sergey Onufrienko <sergey@onufrienko.com>
2022-05-29 21:57:51 +0300

    Added AppState

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
-use actix_web::{middleware, App, HttpServer};
-use std::time::Duration;
+use actix_web::{middleware, web, App, HttpServer};
+use std::{sync::Mutex, time::Duration};
+
+mod handlers;
+mod models;
+mod routes;
+mod state;
+
+use routes::all_routes;
+use state::AppState;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
+    // Shared data across threads
+    let shared_data = web::Data::new(AppState {
+        health_check_response: String::from("OK"),
+        visit_count: Mutex::new(0),
+    });
+
     // Construct App and routes
-    let app = move || App::new().wrap(middleware::Logger::default());
+    let app = move || {
+        App::new()
+            .app_data(shared_data.clone())
+            .wrap(middleware::Logger::default())
+            .configure(all_routes)
+    };
 
     // Start HTTP server
     HttpServer::new(app)
