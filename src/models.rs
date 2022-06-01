62988d662f46b943a62ad3f1ff49216fdf512558
Sergey Onufrienko <sergey@onufrienko.com>
2022-06-01 22:54:52 +0300

    Added derive derive

--- src/models.rs
+++ src/models.rs
@@ -1,11 +1,11 @@
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, sqlx::FromRow)]
 pub struct Device {
     pub device_id: String,
     pub name: String,
-    pub registered_at: Option<NaiveDateTime>,
+    pub registered_at: NaiveDateTime,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -19,10 +19,4 @@ pub struct DeviceData {
 pub struct DeviceConfiguration {
     pub version: u32,
     pub changed_at: Option<NaiveDateTime>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Ping {
-    pub message: String,
-    pub id: i32,
 }
